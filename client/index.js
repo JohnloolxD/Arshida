2c79126f4a5519d18106aaa375645a79ae0bb0b3
JohnloolxD <johdrodc@gmail.com>
2025-05-27 22:48:31 -0400

    Update index.js

--- client/index.js
+++ client/index.js
@@ -7,10 +7,17 @@ function changeMessage() {
         {text:"Pleasee"},
         {text:"Are u dying?"},
         {text:"(Any sentence)."},
-        {text:"I'm so mad rn"}
+        {text:"I'm so mad rn"},
+        {text:"If you got this far...."},
+        {text:"It's you loving dogs!", image: "dog1.jpg"},
+        {text:"It's you loving dogs!", image: "dog2.jpg"},
+        {text:"My fav pic:D", image:"sky.jpg"}
     ];
     if(i >= messages.length){
         i = 0;
+        const box = document.getElementById('messageBox');
+        box.textContent = "That's all I could find in terms of words you used and pics;(";
+        return;
     }
     const message = messages[i++];
     const box = document.getElementById('messageBox');
